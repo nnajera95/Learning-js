1ae215122ef747a95a4fcd1c15dea324f069f57a
nnajera95 <NathanNajera@yahoo.com>
2021-08-01 17:08:22 -0500

    created the initial for loop
    if applied this commit will construct the initial for loop, for the array of pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,14 @@ let pokemonList = [
 {name: 'Arceus', height: 3.2, types: ['normal']},
 {name: 'Blaziken', height: 1.9, types: ['fire', 'fighting']}
 ];
+
+
+for (let i = 0; i < pokemonList[i].height; i++){
+	if (pokemonList[i].height <1 && pokemonList[i].height >.5){
+		document.write(pokemonList[i].name + pokemonList[i].height);
+	} else if (pokemonList[i].height > 3){
+		document.write(pokemonList[i].name + pokemonList[i].height);
+	} else {
+		document.write(pokemonList[i].name + pokemonList[i].height);
+	}
+}
