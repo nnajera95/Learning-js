718f03068661d59d739b3aed3e33b84553479f01
nnajera95 <NathanNajera@yahoo.com>
2021-08-01 17:23:43 -0500

    rearranged the array
    if applied this commit will rearrange the array to work better with the for loop written

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
+{name: 'Blaziken', height: 1.9, types: ['fire', 'fighting']},
 {name: 'Pikachu', height: .4, types: ['electric']},
-{name: 'Arceus', height: 3.2, types: ['normal']},
-{name: 'Blaziken', height: 1.9, types: ['fire', 'fighting']}
+{name: 'Arceus', height: 3.2, types: ['normal']}
 ];
 
 
