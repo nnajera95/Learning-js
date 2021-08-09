2c392be817f23e97205d6ea59931739972dc866d
nnajera95 <NathanNajera@yahoo.com>
2021-08-09 15:27:30 -0500

    wrapped pokemonList in an IIFE
    if applied this commit will wrap the pokemonList array in an IIFE function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
+let pokemonRepository = (function () {
 let pokemonList = [
 {name: 'Pikachu', height: .4, types: ['electric']},
 {name: 'Arceus', height: 3.2, types: ['normal']},
 {name: 'Blaziken', height: 1.9, types: ['fire', 'fighting']}
 ];
+
+return {
+	add: function(pokemon) {
+		pokemonList.push(pokemon);
+	},
+	getAll: function() {
+		return pokemonList;
+	}
+};
+})();
 
 function myLoopFunction(pokemon) {
 	document.write(pokemon.name + ' height:' + pokemon.height + 'm' + '<br/>');
