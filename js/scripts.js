2621b3609b763fa63d3913c2df4edae9828d84f6
nnajera95 <NathanNajera@yahoo.com>
2021-08-14 16:46:07 -0500

    emptied foreach loop block
    if applied this commit will empty the foreach loop and add in the queryselector element and create element for the unordered and ordered list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
+let pokemonRepository = (function (){
 let pokemonList = [
 {name: 'Pikachu', height: .4, types: ['electric']},
 {name: 'Arceus', height: 3.2, types: ['normal']},
 {name: 'Blaziken', height: 1.9, types: ['fire', 'fighting']}
 ];
+
+function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+return {
+	add: add,
+	getAll: getAll
+	
+};
+})();
+
+const pokemonList = pokemonRepository.getAll();
+
+function myLoopFunction(pokemon) {
+	document.querySelector('ul')
+	let listItem = document.createElement('li');
+}
+pokemonList.forEach(myLoopFunction);
