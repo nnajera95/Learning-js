c82c567716d7b59c8154a62215f48971a3d49f59
nnajera95 <NathanNajera@yahoo.com>
2021-08-23 14:15:17 -0500

    removed objects in array
    if applied this commit will remove the objects in the array and leave it empty

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
 const pokemonRepository = (function (){
-const pokemonList = [
-{name: 'Pikachu', height: .4, types: ['electric']},
-{name: 'Arceus', height: 3.2, types: ['normal']},
-{name: 'Blaziken', height: 1.9, types: ['fire', 'fighting']}
-];
+const pokemonList = [];
 let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
 function getAll() {
